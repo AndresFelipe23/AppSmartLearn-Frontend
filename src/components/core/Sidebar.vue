e411c30dce279e7dfd63712bec214b5736dcea91
AndresEspitiaU <75282613+AndresFelipe23@users.noreply.github.com>
2023-11-14 18:34:24 -0500

    update frontend

--- src/components/core/Sidebar.vue
+++ src/components/core/Sidebar.vue
@@ -1,101 +1,132 @@
 <template>
-    <v-navigation-drawer v-model="drawer" app class="fon" width="320px">
-        <vuescroll :ops="ops"> 
+  <v-navigation-drawer v-model="drawer" app class="fon" width="320px">
+    <vuescroll :ops="ops">
 
-    <v-img contain height="120px" src="../../assets/logos/Logo_BN.svg">
-    </v-img>
-  <div style=" text-align: center;"> <label class="font-weight-bold letra text-h5 mx-2" for="">AULAS INTELIGENTES</label>
-  </div> <v-divider>
-    </v-divider>
+      <v-img contain height="120px" src="../../assets/logos/Logo_BN.svg">
+      </v-img>
+      <div style=" text-align: center;"> <label class="font-weight-bold letra text-h5 mx-2" for="">AULAS
+          INTELIGENTES</label>
+      </div> <v-divider>
+      </v-divider>
 
-    <div class="app-sidebar-content">
-            <sidebar-menu showOneChild :menu="menu"/>
-    </div>
-</vuescroll>
+      <div class="app-sidebar-content">
+        <sidebar-menu showOneChild :menu="menu" />
+      </div>
+    </vuescroll>
 
-    </v-navigation-drawer>
-
+  </v-navigation-drawer>
 </template>
 
 <script>
-import {SidebarMenu} from 'vue-sidebar-menu'
+import { SidebarMenu } from 'vue-sidebar-menu'
 import vuescroll from "vuescroll";
 
 
-  export default {
-    components: {
-            SidebarMenu,
-            vuescroll
-        },
+export default {
+  components: {
+    SidebarMenu,
+    vuescroll
+  },
 
-    props:['drawer'],
-    data: () => ({
-    
+  props: ['drawer'],
+  data: () => ({
+
     ops: {
-       scrollPanel: {
-       initialScrollY: false,
-    initialScrollX: false,
-    scrollingX: false,
-    scrollingY: true,
-    speed: 300,
-    easing: undefined,
-    verticalNativeBarPos: 'right'
-          },
-          rail: {
-            
-            background: 'rgba(0, 0, 0, 0.158)',
-    opacity: 1,
-    size: '10px',
-    specifyBorderRadius: false,
-    gutterOfEnds: null,
-     gutterOfSide: '2px',
-    keepShow: false
-          },
-          bar: {
-            showDelay: 500,
-    onlyShowBarOnScroll: true,
-    keepShow: false,
-    background: '#0378a677',
-    opacity: 1,
-    hoverStyle: false,
-    specifyBorderRadius: false,
-    minSize: 0,
-    size: '9px',
-    disable: false
-          },
-        },   
-    
+      scrollPanel: {
+        initialScrollY: false,
+        initialScrollX: false,
+        scrollingX: false,
+        scrollingY: true,
+        speed: 300,
+        easing: undefined,
+        verticalNativeBarPos: 'right'
+      },
+      rail: {
+
+        background: 'rgba(0, 0, 0, 0.158)',
+        opacity: 1,
+        size: '10px',
+        specifyBorderRadius: false,
+        gutterOfEnds: null,
+        gutterOfSide: '2px',
+        keepShow: false
+      },
+      bar: {
+        showDelay: 500,
+        onlyShowBarOnScroll: true,
+        keepShow: false,
+        background: '#0378a677',
+        opacity: 1,
+        hoverStyle: false,
+        specifyBorderRadius: false,
+        minSize: 0,
+        size: '9px',
+        disable: false,
+        userData: null
+      },
+    },
+
     links: [
-        ['mdi-inbox-arrow-down', 'Inbox'],
-        ['mdi-send', 'Send'],
-        ['mdi-delete', 'Trash'],
-        ['mdi-alert-octagon', 'Spam'],
-      ],
+      ['mdi-inbox-arrow-down', 'Inbox'],
+      ['mdi-send', 'Send'],
+      ['mdi-delete', 'Trash'],
+      ['mdi-alert-octagon', 'Spam'],
+    ],
 
-    menu: [],            
-    }),
-    mounted(){
-        this.menu=this.$store.state.userData.menu
-        
-},
-  }
-   </script>
-<style scoped>
-    .fon{
-    /* background-image:url("../../assets/images/sidebar/abstract4.jpg");*/
-    background-image:url("../../assets/fondosidebar.png");
-    background-size: 100% 100%;
-    background-attachment: fixed;
-    width: 100%;
-    height: 100%;
-    margin:0;
+    menu: [],
+  }),
+  mounted() {
+    const rolEspecifico = "638d53bbfe21f8063ca25b93";
+    const isEditor = this.$store.state.userData.isEditor;
+    const isRedactor = this.$store.state.userData.isRedactor;
+    this.menu = this.$store.state.userData.menu;
 
+    console.log("rol", this.$store.state.userData.roles);
+    console.log("editor", this.$store.state.userData.isEditor);
+    console.log("redactor", this.$store.state.userData.isRedactor);
+
+    if (this.$store.state.userData.roles.includes(rolEspecifico)) {
+      if (isEditor && isRedactor) {
+        this.menu = this.$store.state.userData.menu;
+      } else {
+        if (!isEditor && !isRedactor) {
+          this.menu = this.menu.filter(item => item.title !== 'Noticias')
+
+        } else {
+          if (isEditor) {
+            this.menu[2].child = this.menu[2].child.filter(item => item.title !== 'Crear' && item.title !== 'Listar');
+          }
+
+          if (isRedactor) {
+            this.menu[2].child = this.menu[2].child.filter(item => item.title !== 'Editar');
+          }
+        }
+      }
+    } else {
+      this.menu = this.$store.state.userData.menu;
     }
-    .letra{
-      color: #FFFFFF;
-      font-size: 20px;
-     /* font-family: "Mystery Quest";*/
-      font-weight: 400;
-     
-     }
+  }
+
+
+}
+</script>
+<style scoped>
+.fon {
+  /* background-image:url("../../assets/images/sidebar/abstract4.jpg");*/
+  background-image: url("../../assets/fondosidebar.png");
+  background-size: 100% 100%;
+  background-attachment: fixed;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+
+}
+
+.letra {
+  color: #FFFFFF;
+  font-size: 20px;
+  /* font-family: "Mystery Quest";*/
+  font-weight: 400;
+
+}
 </style>
---
+++ src/views/listar/news.vue
@@ -0,0 +1,341 @@
+<template>
+  <div class="noticias-lista">
+    <div class="filtro">
+      <input v-model="busqueda" type="text" class="v-input" placeholder="Buscar noticias" @input="realizarBusqueda">
+      <select v-model="filtroCategoria" class="v-select categoria-select" @change="busquedaCategoria">
+        <option value="">Todas las categorías</option>
+        <option v-for="(categoria, index) in categorias" :key="index">{{ categoria }}</option>
+      </select>
+      <input v-model="filtroFecha" type="date" class="v-input" @change="busquedaFecha">
+    </div>
+    <br>
+    <div>
+      <div class="tabla-responsive">
+        <table>
+          <thead>
+            <tr>
+              <th>Autor</th>
+              <th>Categoría de la noticia</th>
+              <th>Fecha de publicación</th>
+              <th>Días de publicación</th>
+              <th>Estado de la noticia</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(noticia, index) in noticias" :key="index">
+              <td>{{ noticia.autor }}</td>
+              <td>{{ noticia.category }}</td>
+              <td>{{ noticia.time }}</td>
+              <td>{{ noticia.diasPublicacion }}</td>
+              <td>{{ getStatus(noticia.estado) }}</td>
+              <td>
+                <v-btn color="primary" v-if="noticia.estado === '6502fc246f68e90bf2ac9222'" @click="abrirModal(index)"
+                  class="boton-editar">
+                  <b-icon icon="Eye-fill" font-scale="2" style="color:rgb(255, 255, 255)"></b-icon> Comentarios </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <vs-dialog v-if="modalActivo" not-center v-model="modalActivo" class="dialog">
+        <template #header>
+        </template>
+        <template #default>
+          <div class="con-content">
+            {{ noticiaSeleccionada.comentario }}
+          </div>
+        </template>
+        <template #footer>
+          <div class="con-footer d-flex justify-content-end">
+            <router-link :to="'/dashboard/welcome/creanotice/' + noticiaSeleccionada._id"> <vs-button class="mr-2"
+                color="#00BA00">Editar noticia</vs-button></router-link>
+            <vs-button color="#FF3232" @click="borrar(index)">Cancelar noticia</vs-button>
+          </div>
+        </template>
+
+      </vs-dialog>
+
+      <vs-dialog v-if="modalImage" not-center v-model="modalImage" class="dialog">
+        <template #default>
+          <div class="con-content">
+            <v-img height="auto" :src="image"></v-img>
+          </div>
+        </template>
+
+      </vs-dialog>
+      <transition name="modal">
+        <vs-dialog v-if="showModal" v-model="showModal" hide-header-close>
+          <div class="con-content">
+            {{ mensaje }}
+          </div>
+        </vs-dialog>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      busqueda: '',
+      filtroCategoria: '',
+      filtroFecha: '',
+      noticias: [],
+      noticiasporDefecto: [],
+      categorias: [],
+      idNoticia: '',
+      modalActivo: false,
+      modalImage: false,
+      noticiaSeleccionada: null,
+      image: null,
+      currentIndex: null,
+      mensaje: '',
+      showModal: false,
+    };
+  },
+  mounted() {
+    this.getNotices()
+    this.userid = this.$store.state.userData._id
+    console.log('userid es', this.userid)
+
+
+
+  },
+  computed: {
+    processedAbstract() {
+      return this.noticiaSeleccionada.abstract.replace(/<strong>/g, '<b>').replace(/<\/strong>/g, '</b>');
+    },
+    processedContent() {
+      return this.noticiaSeleccionada.content.replace(/<strong>/g, '<b>').replace(/<\/strong>/g, '</b>');
+    }
+  },
+
+  methods: {
+    getNotices() {
+      axios
+        .get('http://localhost:3001/notices')
+        .then((response) => {
+          this.noticias = response.data.filter(noticia => noticia.idAutor === this.$store.state.userData._id);
+          this.noticiasporDefecto = [...response.data];
+          this.categorias = this.obtenerCategorias(response.data); // Obtener categorías únicas
+          console.log(this.noticias);
+        })
+        .catch((error) => {
+          console.error('Error al obtener noticias', error);
+        });
+    },
+
+
+    getStatus(estado) {
+      console.log('estado', estado)
+
+      switch (estado) {
+        case "6502fbb06f68e90bf2ac9220":
+          return 'Redactado';
+        case "6502fc146f68e90bf2ac9221":
+          return 'Publicado';
+        case "6502fc246f68e90bf2ac9222":
+          return 'Rechazada';
+        default:
+          return 'Estado de noticia desconocido';
+      }
+    },
+
+    abrirModal(index) {
+      this.noticiaSeleccionada = this.noticias[index]
+      this.currentIndex = index;
+      console.log('noticiaSeleccionada', this.noticiaSeleccionada)
+      console.log('currentIndex', this.currentIndex)
+      this.modalActivo = true
+    },
+
+    abrirModalImagen() {
+      this.image = this.noticiaSeleccionada.images;
+      this.modalImage = true;
+    },
+
+    editar() {
+      if (this.currentIndex !== null) {
+        this.idNoticia = this.noticias[this.currentIndex]._id
+        console.log(this.currentIndex)
+        console.log(this.idNoticia)
+
+        axios.put(`http://localhost:3001/noticias/${this.idNoticia}`, {
+          aprobada: true
+        })
+          .then(response => {
+            console.log('noticia aprobada', response)
+            this.mensaje = 'Noticia aprobada'
+            this.showModal = true
+
+            setTimeout(() => {
+              this.showModal = false;
+              this.mensaje = '';
+            }, 3000);
+          })
+          .catch(error => {
+            console.error(error);
+          })
+      }
+    },
+
+    realizarBusqueda() {
+      if (this.busqueda) {
+        this.noticias = this.noticiasporDefecto.filter(noticia =>
+          Object.values(noticia).some(valor =>
+            String(valor).toLowerCase().includes(this.busqueda.toLowerCase())
+          )
+        );
+      } else {
+        this.noticias = [...this.noticiasporDefecto];
+      }
+    },
+    busquedaCategoria() {
+      if (this.filtroCategoria) {
+        this.noticias = this.noticiasporDefecto.filter((noticia) =>
+          noticia.category.toLowerCase().includes(this.filtroCategoria.toLowerCase())
+        );
+      } else {
+        this.noticias = [...this.noticiasporDefecto];
+      }
+    },
+    busquedaFecha() {
+      if (this.filtroFecha) {
+        this.noticias = this.noticiasporDefecto.filter((noticia) =>
+          noticia.fechaPublicacion.includes(this.filtroFecha)
+        );
+      } else {
+        this.noticias = [...this.noticiasporDefecto];
+      }
+    },
+    obtenerCategorias(noticias) {
+      // Función para obtener categorías únicas de las noticias
+      const categoriasUnicas = new Set();
+      noticias.forEach((noticia) => {
+        categoriasUnicas.add(noticia.category);
+      });
+      return Array.from(categoriasUnicas);
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+
+.dialog {
+  width: 100%;
+
+}
+
+.con-content td {
+  overflow: auto;
+}
+
+th,
+td {
+  border: 1px solid #ccc;
+  text-align: center;
+  padding: 10px;
+}
+
+th {
+  background-color: #0079c4;
+  /* Cambia el color de fondo del encabezado */
+  color: white;
+  /* Cambia el color del texto del encabezado */
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.3s;
+}
+
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
+
+.v-input,
+.v-select {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-right: 10px;
+}
+
+.v-input[type="date"] {
+  padding: 6px;
+}
+
+.v-select {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-right: 10px;
+  width: 200px;
+
+}
+
+.categoria-select {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 8px;
+  width: 12%;
+}
+
+.noticias-lista {
+  text-align: center;
+  padding: 20px;
+}
+
+.filtro {
+  display: flex;
+  justify-content: center;
+}
+
+.filtro input,
+select {
+  margin-right: 40px;
+  margin-left: 40px;
+
+}
+
+.boton-editar,
+.boton-borrar {
+  background-color: green;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  margin: 2px;
+  cursor: pointer;
+  border-radius: 5px;
+  font-size: 14px;
+}
+
+.boton-editar:hover,
+.boton-borrar:hover {
+  background-color: darkgreen;
+}
+
+.boton-borrar {
+  background-color: red;
+}
+
+.boton-borrar:hover {
+  background-color: darkred;
+}</style>
