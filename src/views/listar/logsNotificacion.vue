0206b0d56c460fcfd8ed0ef4602d6a9bb39f8b5f
AndresEspitiaU <75282613+AndresFelipe23@users.noreply.github.com>
2023-11-16 22:52:58 -0500

    new id sms

--- src/views/listar/logsNotificacion.vue
+++ src/views/listar/logsNotificacion.vue
@@ -1,45 +1,41 @@
 <template>
-    <div>
-      <v-data-table
-        :headers="headers"
-        :items="logs"
-        class="elevation-1"
-      >
-        <template v-slot:items="props">
-          <td>{{ props.item.receptor }}</td>
-          <td class="text-xs-right">{{ props.item.fecha }}</td>
-          <td class="text-xs-right">{{ props.item.detalles }}</td>
-        </template>
-      </v-data-table>
-    </div>
-  </template>
+  <div>
+    <v-data-table :headers="headers" :items="logs" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.receptor }}</td>
+        <td class="text-xs-right">{{ props.item.fecha }}</td>
+        <td class="text-xs-right">{{ props.item.detalles }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
   
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    data () {
-      return {
-        logs: [], 
-        headers: [
-          { text: 'Receptor', value: 'receptor' },
-          { text: 'Fecha de envío', value: 'fecha' },
-          { text: 'Detalles', value: 'detalles' },
-        ],
-      }
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      logs: [],
+      headers: [
+        { text: 'Receptor', value: 'receptor' },
+        { text: 'Fecha de envío', value: 'fecha' },
+        { text: 'Detalles', value: 'detalles' },
+      ],
+    }
+  },
+  methods: {
+    cargarLogs() {
+      axios
+        .get('http://localhost:3001/logs')
+        .then((response) => {
+          this.logs = response.data;
+          console.log('logs', this.logs);
+        })
+        .catch((error) => {
+          console.error('Error al obtener logs', error);
+        });
     },
-    methods: {
-      cargarLogs() {
-        axios
-          .get('http://localhost:3001/logs')
-          .then((response) => {
-            this.logs = response.data;
-            console.log('logs', this.logs);
-          })
-          .catch((error) => {
-            console.error('Error al obtener logs', error);
-          });
-      },
     //   getStatusClass(statusHttp) {
     //     return {
     //         'text-xs-right': true,
@@ -47,20 +43,20 @@
     //         'bg-green': statusHttp === '200',
     //     }
     //     },
-    },
-    mounted() {
-        this.cargarLogs();
-    }
+  },
+  mounted() {
+    this.cargarLogs();
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-  .bg-red {
-    background-color: #ff6767;
-  }
-  
-  .bg-green {
-    background-color: #78ff78;
-  }
-  </style>
+<style scoped>
+.bg-red {
+  background-color: #ff6767;
+}
+
+.bg-green {
+  background-color: #78ff78;
+}
+</style>
   
